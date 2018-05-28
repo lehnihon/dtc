c11a77214836bd6c0514fa5ee31c2c27c0731e04
Leandro <lehnihon@hotmail.com>
2018-05-28 16:43:13 -0300

    Inclusão de template admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PesquisarComponent } from './pesquisar/pesquisar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'pesquisar', component: PesquisarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PesquisarComponent } from './pesquisar/pesquisar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PesquisarComponent
   ],
   imports: [
     BrowserModule,
